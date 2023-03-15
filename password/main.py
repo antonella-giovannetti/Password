521961a157a7b95638583c0930d5779f9f6ef898
antonella-giovannetti <127109361+antonella-giovannetti@users.noreply.github.com>
2023-03-15 13:21:53 +0100

    thrid commit

--- password/main.py
+++ password/main.py
@@ -2,6 +2,39 @@ import hashlib
 import json
 import os.path
 
+def menu():
+    while True:
+        print(' ')
+        print('MENU')
+        print('1 - Créer un compte')
+        print('2 - Afficher les utilisateurs')
+        print('3 - Quitter')
+        print(' ')
+        number = input('Choissisez un chiffre > ')
+        print(' ')
+        if number.isdigit() == False:
+            print(' ')
+            print('Veuillez entrer un chiffre entre 1 et 3!')
+            print(' ')
+        elif number == "1":
+            create_account()
+        elif number == "2":
+            if os.path.isfile("data.json"):
+                with open('data.json', 'r') as file_json:
+                    dict_json = json.load(file_json)
+                    print('Voici les utilisateurs enregistrés : ')
+                    for i in dict_json["infos"]:
+                        print(i['user'] + ": " + i['password'])
+                    print(' ')
+            else:
+                print('Il n\'y a pas encore de compte enregistré')
+        elif number == "3":
+            break
+        else:
+            print(' ')
+            print('Veuillez entrer un chiffre valide !')
+            print(' ')
+            
 # Fonction pour ajouter données dans un fichier json
 def add_on_json(user, password):
     if os.path.isfile("data.json"):
@@ -11,9 +44,13 @@ def add_on_json(user, password):
             file_json.truncate(0)
             file_json.seek(0)
             json.dump((dict_json), file_json, indent=4)
+            print(' ')
+            print('Votre compte a bien été enregistré')
     else:
         with open('data.json', 'w') as file_json:
             json.dump({"infos": [{"user": user, "password" : password}]}, file_json, indent=4)
+        print(' ')
+        print('Votre compte a bien été enregistré')
 
 # Fonction pour hasher le mdp
 def hashed_password(password):
@@ -24,18 +61,15 @@ def hashed_password(password):
     return new_password
 
 # Fonction pour création de mdp et vérification
-def create_user_with_password():
-    hashed_pswd = ""
+def create_account():
     while True:
-        user = input('Créer un nom d\'utilisateur : ')
+        user = input('Veuillez entrer un nom d\'utilisateur : ')
         if os.path.isfile("data.json"):
             with open('data.json', 'r') as file_json:
                 dict_json = json.load(file_json)
                 # Vérifier si l'utilisateur existe déjà dans le dictionnaire
-                for info in dict_json["infos"]:
-                    if info["user"] == user:
-                        print("L'utilisateur existe déjà.")
-                        break
+                if user in [info["user"] for info in dict_json["infos"]]:
+                    print("L'utilisateur existe déjà.")
                 else:
                     # L'utilisateur n'existe pas encore
                     break
@@ -44,7 +78,7 @@ def create_user_with_password():
             break
 
     while True:
-        password = input('Créer votre mot de passe : ')
+        password = input('Veuillez entrer un mot de passe : ')
         nbr_maj = 0
         nbr_min = 0
         nbr_chiffre = 0
@@ -63,9 +97,19 @@ def create_user_with_password():
         if len(password) < 8 or nbr_maj < 1 or nbr_min < 1 or nbr_chiffre < 1 or nbr_character < 1:
             print('Votre mot de passe doit contenir au moins 8 caractères,une lettre en majuscule et minuscule, un chiffre et un caractère spécial(!, @, #, $, %, ^, &, *).')
         else:
-            print('ok')
             hashed_pswd = hashed_password(password)
+            if os.path.isfile("data.json"):
+                with open('data.json', 'r') as file_json:
+                    dict_json = json.load(file_json)
+                    # Vérifier si l'utilisateur existe déjà dans le dictionnaire
+                    if hashed_pswd in [info["password"] for info in dict_json["infos"]]:
+                        print("Le mot de passe existe déjà.")
+                    else:
+                        add_on_json(user, hashed_pswd)
+                        break
+            else:
+                # Fichier JSON n'existe pas encore
+                add_on_json(user, hashed_pswd)
+                break
             break
-    add_on_json(user, hashed_pswd)
-
-create_user_with_password()
+menu()
